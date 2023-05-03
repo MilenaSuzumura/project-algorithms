a3e540fc3672a1b090dd7542fc5faa0138d530ac
Milena Suzumura <milena.suzumura11@gmail.com>
2023-05-02 22:17:07 -0300

    requisito 3: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    else:
+        if word[high_index] != word[low_index]:
+            return False
+
+        if low_index >= high_index:
+            return True
+
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
