b1a8ad5eb917b8cf0926aea0a79c61decb487b76
Milena Suzumura <milena.suzumura11@gmail.com>
2023-05-03 14:24:09 -0300

    Tentei fazer a função virar O(n log n)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,26 +1,23 @@
 def sort_list(characters: list[str]):
     for i in range(len(characters)):
-        min = i
+        value = characters[i]
+        position = i
 
-        for j in range(i + 1, len(characters)):
-            if characters[j] < characters[min]:
-                min = j
-        characters[min], characters[i] = characters[i], characters[min]
+        while (position > 0) and (characters[position - 1] > value):
+            characters[position] = characters[position - 1]
+            position = position - 1
+        characters[position] = value
     return characters
 
 
-def list_strings(word: str):
-    list_string = list(word.lower())
-    sort_word = sort_list(list_string)
-    return sort_word
-
-
 def is_anagram(first_string, second_string):
-    if not first_string or not second_string:
-        return (first_string, second_string, False)
+    list_first_string = list(first_string.lower())
+    list_second_string = list(second_string.lower())
 
-    list_first_string = list_strings(first_string)
-    list_second_string = list_strings(second_string)
-    first = ''.join(list_first_string)
-    second = ''.join(list_second_string)
+    first = ''.join(sort_list(list_first_string))
+    second = ''.join(sort_list(list_second_string))
+
+    if len(first) == 0 or len(second) == 0:
+        return (first, second, False)
+    
     return (first, second, list_first_string == list_second_string)
